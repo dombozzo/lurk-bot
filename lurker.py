9e1ddc0c43d6d1af39b7bfa71cf623ad54d5f34f
Dominic Bozzo <dbozzo@nd.edu>
2018-04-24 23:25:56 -0400

    ready for test 2

--- lurker.py
+++ lurker.py
@@ -2,7 +2,8 @@
 import sys
 import crawler
 import build_graph
+import plot_graph
 
 tempgraph = crawler.crawl(sys.argv)
 graph = build_graph.build(tempgraph)
-print(graph)
+plot_graph.make_plot(graph)
--- plot_graph.py
+++ plot_graph.py
@@ -6,112 +6,113 @@ from plotly.graph_objs import *
 import json
 import urllib2
 
-data = []
-req = urllib2.Request("https://raw.githubusercontent.com/plotly/datasets/master/miserables.json")
-opener = urllib2.build_opener()
-f = opener.open(req)
-data = json.loads(f.read())
+def make_plot(data):
+    # data = []
+    # req = urllib2.Request("https://raw.githubusercontent.com/plotly/datasets/master/miserables.json")
+    # opener = urllib2.build_opener()
+    # f = opener.open(req)
+    # data = json.loads(f.read())
 
-print data.keys()
+    print data.keys()
 
-# get num of nodes
+    # get num of nodes
 
-N=len(data['nodes'])
+    N=len(data['nodes'])
 
-# define list of edges
+    # define list of edges
 
-L=len(data['links'])
-Edges=[(data['links'][k]['source'], data['links'][k]['target']) for k in range(L)]
+    L=len(data['links'])
+    Edges=[(data['links'][k]['source'], data['links'][k]['target']) for k in range(L)]
 
-G=ig.Graph(Edges, directed=False)
+    G=ig.Graph(Edges, directed=False)
 
-#extract group and __name__
+    #extract group and __name__
 
-labels=[]
-group=[]
-for node in data['nodes']:
-    labels.append(node['name'])
-    group.append(node['group'])
+    labels=[]
+    group=[]
+    for node in data['nodes']:
+        labels.append(node['name'])
+        group.append(node['group'])
 
-# get node positions
+    # get node positions
 
-layt=G.layout('kk', dim=3)
+    layt=G.layout('kk', dim=3)
 
-# set data for plotly
+    # set data for plotly
 
-Xn=[layt[k][0] for k in range(N)]# x-coordinates of nodes
-Yn=[layt[k][1] for k in range(N)]# y-coordinates
-Zn=[layt[k][2] for k in range(N)]# z-coordinates
-Xe=[]
-Ye=[]
-Ze=[]
-for e in Edges:
-    Xe+=[layt[e[0]][0],layt[e[1]][0], None]# x-coordinates of edge ends
-    Ye+=[layt[e[0]][1],layt[e[1]][1], None]
-    Ze+=[layt[e[0]][2],layt[e[1]][2], None]
+    Xn=[layt[k][0] for k in range(N)]# x-coordinates of nodes
+    Yn=[layt[k][1] for k in range(N)]# y-coordinates
+    Zn=[layt[k][2] for k in range(N)]# z-coordinates
+    Xe=[]
+    Ye=[]
+    Ze=[]
+    for e in Edges:
+        Xe+=[layt[e[0]][0],layt[e[1]][0], None]# x-coordinates of edge ends
+        Ye+=[layt[e[0]][1],layt[e[1]][1], None]
+        Ze+=[layt[e[0]][2],layt[e[1]][2], None]
 
-trace1=Scatter3d(x=Xe,
-               y=Ye,
-               z=Ze,
-               mode='lines',
-               line=Line(color='rgb(125,125,125)', width=1),
-               hoverinfo='none'
-               )
-trace2=Scatter3d(x=Xn,
-               y=Yn,
-               z=Zn,
-               mode='markers',
-               name='actors',
-               marker=Marker(symbol='dot',
-                             size=6,
-                             color=group,
-                             colorscale='Viridis',
-                             line=Line(color='rgb(50,50,50)', width=0.5)
-                             ),
-               text=labels,
-               hoverinfo='text'
-               )
+    trace1=Scatter3d(x=Xe,
+                   y=Ye,
+                   z=Ze,
+                   mode='lines',
+                   line=Line(color='rgb(125,125,125)', width=1),
+                   hoverinfo='none'
+                   )
+    trace2=Scatter3d(x=Xn,
+                   y=Yn,
+                   z=Zn,
+                   mode='markers',
+                   name='actors',
+                   marker=Marker(symbol='dot',
+                                 size=6,
+                                 color=group,
+                                 colorscale='Viridis',
+                                 line=Line(color='rgb(50,50,50)', width=0.5)
+                                 ),
+                   text=labels,
+                   hoverinfo='text'
+                   )
 
-axis=dict(showbackground=False,
-          showline=False,
-          zeroline=False,
-          showgrid=False,
-          showticklabels=False,
-          title=''
-          )
+    axis=dict(showbackground=False,
+              showline=False,
+              zeroline=False,
+              showgrid=False,
+              showticklabels=False,
+              title=''
+              )
 
-layout = Layout(
-         title="Network of coappearances of characters in Victor Hugo's novel<br> Les Miserables (3D visualization)",
-         width=1000,
-         height=1000,
-         showlegend=False,
-         scene=Scene(
-         xaxis=XAxis(axis),
-         yaxis=YAxis(axis),
-         zaxis=ZAxis(axis),
+    layout = Layout(
+             title="Network of coappearances of characters in Victor Hugo's novel<br> Les Miserables (3D visualization)",
+             width=1000,
+             height=1000,
+             showlegend=False,
+             scene=Scene(
+             xaxis=XAxis(axis),
+             yaxis=YAxis(axis),
+             zaxis=ZAxis(axis),
+            ),
+         margin=Margin(
+            t=100
         ),
-     margin=Margin(
-        t=100
-    ),
-    hovermode='closest',
-    annotations=Annotations([
-           Annotation(
-           showarrow=False,
-            text="Data source: <a href='http://bost.ocks.org/mike/miserables/miserables.json'>[1] miserables.json</a>",
-            xref='paper',
-            yref='paper',
-            x=0,
-            y=0.1,
-            xanchor='left',
-            yanchor='bottom',
-            font=Font(
-            size=14
-            )
-            )
-        ]),    )
+        hovermode='closest',
+        annotations=Annotations([
+               Annotation(
+               showarrow=False,
+                text="Data source: <a href='http://bost.ocks.org/mike/miserables/miserables.json'>[1] miserables.json</a>",
+                xref='paper',
+                yref='paper',
+                x=0,
+                y=0.1,
+                xanchor='left',
+                yanchor='bottom',
+                font=Font(
+                size=14
+                )
+                )
+            ]),    )
 
 
-data=Data([trace1, trace2])
-fig=Figure(data=data, layout=layout)
+    data=Data([trace1, trace2])
+    fig=Figure(data=data, layout=layout)
 
-py.iplot(fig, filename='Les-Miserables')
+    py.iplot(fig, filename='Les-Miserables')
